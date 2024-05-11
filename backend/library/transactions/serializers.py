b4aedd55195697bec47cbf00d5e98a9ba621e5d7
Timur <MuratovTim3@gmail.com>
2024-05-11 14:13:46 +0300

    Front-end fixed

--- backend/library/transactions/serializers.py
+++ backend/library/transactions/serializers.py
@@ -3,6 +3,19 @@ from .models import Transaction, Book
 
 
 class TransactionSerializer(serializers.ModelSerializer):
+    customer = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='last_name'
+    )
+    book = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='title'
+    )
+    confirmed_by = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='last_name'
+    )
+
     class Meta:
         model = Transaction
         fields = ["id", "customer", "book", "status", "confirmed_by", "is_expired"]
